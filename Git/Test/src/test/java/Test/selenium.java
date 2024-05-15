cb4c2f6b3a36a35f755e9bd8b340b1a316af0b55
sujith <sujitheee206@gmail.com>
2024-05-15 19:02:08 +0530

    add

---
+++ Git/Test/src/test/java/Test/selenium.java
@@ -0,0 +1,15 @@
+package Test;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.Test;
+
+public class selenium {
+
+		@Test
+		public void Method1() {
+			System.out.println("suji");
+
+	}
+
+}
